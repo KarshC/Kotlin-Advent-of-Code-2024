09e589d42b2b6f503c928fbfa2ac41f013ed71e7
karshC <karshpsu@gmail.com>
2024-12-06 10:14:26 -0500

    day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -10,8 +10,20 @@ fun main() {
         return safeLines
     }
 
-    fun part2(input: List<String>) {
-
+    fun part2(input: List<String>): Int {
+        var result = 0
+        for (line in input) {
+            val numbers = line.split(" ").map { it.toInt() }
+            var isSafe = false
+            for (i in 0..numbers.lastIndex) {
+                isSafe = isLineSafe(numbers.toMutableList().apply { removeAt(i) })
+                if (isSafe) {
+                    result++
+                    break
+                }
+            }
+        }
+        return result
     }
 
     // Read the input from the `src/Day02.txt` file.
